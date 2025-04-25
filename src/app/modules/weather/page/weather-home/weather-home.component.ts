1cbea8191170dbe060ad02fba7e8f7f27888f978
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-04-25 13:01:09 -0300

    feat: add onDestroy

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDatas } from 'src/models/interfaces/WeatherDatas.interface';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
   defaultCityName = "São Paulo";
   weatherDatas!: WeatherDatas;
 
@@ -15,6 +17,11 @@ export class WeatherHomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getWeatherDatas(this.defaultCityName);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   getWeatherDatas(cityName: string): void{
