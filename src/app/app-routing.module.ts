3eba6727a28b439062e00920feced72fe7b087e3
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-04-25 12:10:56 -0300

    feat: first component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherHomeComponent } from './modules/weather/page/weather-home/weather-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: WeatherHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
